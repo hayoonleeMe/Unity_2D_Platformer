1112ed32b7777e412b3faf27072bed888897e8aa
LeeHaYoon <73100820+LeeHaYoon@users.noreply.github.com>
2022-02-12 16:16:24 +0900

    플레이어가 스파이크에 닿으면 일정 시간마다 데미지를 입히도록 하는 코루틴 추가

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -8,19 +8,52 @@ public class Spike : MonoBehaviour
     [SerializeField]
     private float damage = 1.0f;
 
+    // 스파이크가 플레이어에게 데미지를 입히는 딜레이
+    [SerializeField]
+    private float hitDelay = 0.5f;
+
+    // 플레이어 오브젝트
+    private GameObject playerObject = null;
+
+    // 코루틴 RepeatHitProcess 에서 받아온 IEnumerator
+    private IEnumerator repeatHitProcess;
+
+    private void Start()
+    {
+        repeatHitProcess = RepeatHitProcess();  
+    }
+
+    private IEnumerator RepeatHitProcess()
+    {
+        while (true)
+        {
+            if (playerObject != null)
+            {
+                // 플레이어에게 데미지를 입힌다.
+                playerObject.GetComponent<PlayerHP>().TakeDamage(damage);
+            }
+
+            // hitDelay 만큼 대기한다.
+            yield return new WaitForSeconds(hitDelay);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // 스파이크가 플레이어와 부딪힐 때
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && collision.friction != 0)
         {
-            // 플레이어의 Collider 가 frictionless 인 Collider 이므로 제외한다.
-            if (collision.friction == 0)
-            {
-                return;
-            }
-            
-            // 스파이크의 데미지 만큼 플레이어에게 데미지를 입힌다.
-            collision.gameObject.GetComponent<PlayerHP>().TakeDamage(damage);
+            playerObject = collision.gameObject;
+
+            StartCoroutine(repeatHitProcess);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.friction != 0)
+        {
+            StopCoroutine(repeatHitProcess);
+            playerObject = null;
         }
     }
 }
