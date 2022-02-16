91e69aeb31cc6253156e519b1e5d92256e04c94a
LeeHaYoon <73100820+LeeHaYoon@users.noreply.github.com>
2022-02-16 17:50:33 +0900

    카메라의 y축 이동을 제거하고 카메라 오브젝트의 y포지션을 증가시켜 플레이어보다 높은 위치에 고정되도록 함

--- Assets/Scripts/CameraAlignment.cs
+++ Assets/Scripts/CameraAlignment.cs
@@ -36,10 +36,11 @@ public class CameraAlignment : MonoBehaviour
         // 플레이어 오브젝트가 이동한 후 카메라를 플레이어의 위치로 이동시킨다.
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = playerTransform.position.x;
-        cameraPosition.y = playerTransform.position.y + offsetY;
+        //cameraPosition.y = playerTransform.position.y + offsetY;
 
         // 카메가가 최소, 최대 x, y좌표를 넘지 않도록 조정하여 이동시킨다.
-        transform.position = new Vector3(Mathf.Clamp(cameraPosition.x, minLimitX, maxLimitX), 
-                                         Mathf.Clamp(cameraPosition.y, minLimitY, maxLimitY), cameraPosition.z);
+        //transform.position = new Vector3(Mathf.Clamp(cameraPosition.x, minLimitX, maxLimitX), 
+        //                                 Mathf.Clamp(cameraPosition.y, minLimitY, maxLimitY), cameraPosition.z);
+        transform.position = new Vector3(Mathf.Clamp(cameraPosition.x, minLimitX, maxLimitX), cameraPosition.y, cameraPosition.z);
     }
 }
