768debdfcaf129499712e9446fd4759d227ca350
LeeHaYoon <73100820+LeeHaYoon@users.noreply.github.com>
2022-02-28 16:20:26 +0900

    배경 스프라이트를 옮길때 localScale 대신 SpriteRenderer 의 bounds.size 멤버를 사용하도록 수정

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
     // 스테이지의 정보를 담는 객체
     [SerializeField]
     private StageData stageData;
@@ -19,6 +21,11 @@ public class Background : MonoBehaviour
     // 배경 스프라이트를 이동시킬지 결정하는 범위를 정하는 변수
     private float offset = 2.5f;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void Start()
     {
         halfViewPortXSize = (Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f))).x -
@@ -30,19 +37,13 @@ public class Background : MonoBehaviour
         StartCoroutine(CheckAlign());
     }
 
-
-    /// <summary>
-    /// 배경 스프라이트를 옮길때 localScale 대신 SpriteRenderer 의 bounds.size 멤버를 사용하도록 수정
-    /// </summary>
-    /// <returns></returns>
-
     // 배경 스프라이트가 이동해야 하는지 CHECK_SECONDS 마다 체크하는 코루틴 함수
     IEnumerator CheckAlign()
     {
         while (true)
         {
-            float bgEdgeXRightPos = transform.position.x + transform.localScale.x / 2;
-            float bgEdgeXLeftPos = transform.position.x - transform.localScale.x / 2;
+            float bgEdgeXRightPos = transform.position.x + spriteRenderer.bounds.size.x / 2;
+            float bgEdgeXLeftPos = transform.position.x - spriteRenderer.bounds.size.x / 2;
 
             float cameraEdgeXRightPos = Camera.main.transform.position.x + halfViewPortXSize;
             float cameraEdgeXLeftPos = Camera.main.transform.position.x - halfViewPortXSize;
@@ -51,17 +52,17 @@ public class Background : MonoBehaviour
             if (cameraEdgeXRightPos >= bgEdgeXRightPos - offset)
             {
                 Vector3 pos = transform.position;
-                transform.position = new Vector3(cameraEdgeXLeftPos + transform.localScale.x / 2 - offset * 2, pos.y, pos.z);
+                transform.position = new Vector3(cameraEdgeXLeftPos + spriteRenderer.bounds.size.x / 2 - offset * 2, pos.y, pos.z);
             }
             // 플레이어가 왼쪽으로 이동하다가 배경 스프라이트를 옮기는 경우
             else if (cameraEdgeXLeftPos <= bgEdgeXLeftPos + offset)
             {
                 Vector3 pos = transform.position;
-                transform.position = new Vector3(cameraEdgeXRightPos - transform.localScale.x / 2 + offset * 2, pos.y, pos.z);
+                transform.position = new Vector3(cameraEdgeXRightPos - spriteRenderer.bounds.size.x / 2 + offset * 2, pos.y, pos.z);
             }
 
-            float bgEdgeYUpPos = transform.position.y + transform.localScale.y / 2;
-            float bgEdgeYDownPos = transform.position.y - transform.localScale.y / 2;
+            float bgEdgeYUpPos = transform.position.y + spriteRenderer.bounds.size.y / 2;
+            float bgEdgeYDownPos = transform.position.y - spriteRenderer.bounds.size.y / 2;
 
             float cameraEdgeYUpPos = Camera.main.transform.position.y + halfViewPortYSize;
             float cameraEdgeYDownPos = Camera.main.transform.position.y - halfViewPortYSize;
@@ -70,13 +71,13 @@ public class Background : MonoBehaviour
             if (cameraEdgeYUpPos >= bgEdgeYUpPos - offset)
             {
                 Vector3 pos = transform.position;
-                transform.position = new Vector3(pos.x, cameraEdgeYDownPos + transform.localScale.y / 2 - offset * 2, pos.z);
+                transform.position = new Vector3(pos.x, cameraEdgeYDownPos + spriteRenderer.bounds.size.y / 2 - offset * 2, pos.z);
             }
             // 플레이어가 아래쪽으로 이동하다가 배경 스프라이트를 옮기는 경우
             else if (cameraEdgeYDownPos <= bgEdgeYDownPos + offset)
             {
                 Vector3 pos = transform.position;
-                transform.position = new Vector3(pos.x, cameraEdgeYUpPos - transform.localScale.y / 2 + offset * 2, pos.z);
+                transform.position = new Vector3(pos.x, cameraEdgeYUpPos - spriteRenderer.bounds.size.y / 2 + offset * 2, pos.z);
             }
 
             yield return new WaitForSeconds(CHECK_SECONDS);
